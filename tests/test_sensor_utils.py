9c10ae6ab5c749c5617ea07903a4962c1b20aced
prestomation <prestomation@users.noreply.github.com>
2026-03-19 03:17:47 +0000

    fix: universal HA module mock for tests (handles all submodule imports)

--- tests/test_sensor_utils.py
+++ tests/test_sensor_utils.py
@@ -9,94 +9,56 @@ import sys, os
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
 
 # We need to mock homeassistant modules since HA is not installed
+# Use a universal mock that auto-creates submodules on demand
 import types
-
-# Create minimal HA mock so sensor.py can be imported
-ha_mock = types.ModuleType('homeassistant')
-ha_util = types.ModuleType('homeassistant.util')
-ha_dt = types.ModuleType('homeassistant.util.dt')
-ha_components = types.ModuleType('homeassistant.components')
-ha_sensor = types.ModuleType('homeassistant.components.sensor')
-ha_core = types.ModuleType('homeassistant.core')
-ha_config_entries = types.ModuleType('homeassistant.config_entries')
-ha_helpers = types.ModuleType('homeassistant.helpers')
-ha_entity = types.ModuleType('homeassistant.helpers.entity')
-ha_entity_platform = types.ModuleType('homeassistant.helpers.entity_platform')
-ha_update_coordinator = types.ModuleType('homeassistant.helpers.update_coordinator')
-
-# Provide stub classes/values
 import zoneinfo
-
-PACIFIC = zoneinfo.ZoneInfo('America/Los_Angeles')
-ha_dt.DEFAULT_TIME_ZONE = PACIFIC
-ha_dt.now = lambda: datetime.now(PACIFIC)
-
 from enum import Enum
-class SensorStateClass(str, Enum):
+
+class _HAModuleFinder:
+    """Auto-create mock modules for any homeassistant.* import."""
+    _cache = {}
+    
+    @classmethod
+    def find_module(cls, name, path=None):
+        if name == 'homeassistant' or name.startswith('homeassistant.'):
+            return cls
+        return None
+    
+    @classmethod
+    def load_module(cls, name):
+        if name in sys.modules:
+            return sys.modules[name]
+        mod = types.ModuleType(name)
+        # Add commonly needed attributes
+        mod.__path__ = []
+        mod.__package__ = name
+        sys.modules[name] = mod
+        cls._cache[name] = mod
+        return mod
+
+sys.meta_path.insert(0, _HAModuleFinder)
+
+# Pre-populate critical HA mock attributes
+import homeassistant.util.dt as ha_dt
+ha_dt.now = lambda: __import__('datetime').datetime.now(__import__('datetime').timezone.utc)
+ha_dt.DEFAULT_TIME_ZONE = zoneinfo.ZoneInfo('America/Los_Angeles')
+
+import homeassistant.components.sensor as ha_sensor
+class SensorDeviceClass(Enum):
+    TIMESTAMP = 'timestamp'
+    BATTERY = 'battery'
+    TEMPERATURE = 'temperature'
+class SensorStateClass(Enum):
     MEASUREMENT = 'measurement'
     TOTAL = 'total'
-    TOTAL_INCREASING = 'total_increasing'
-class SensorDeviceClass(str, Enum):
-    TIMESTAMP = 'timestamp'
-
-ha_sensor.SensorStateClass = SensorStateClass
 ha_sensor.SensorDeviceClass = SensorDeviceClass
-ha_sensor.SensorEntity = object
-ha_sensor.SensorEntityDescription = object
+ha_sensor.SensorStateClass = SensorStateClass
+ha_sensor.SensorEntity = type('SensorEntity', (), {})
+ha_sensor.SensorEntityDescription = type('SensorEntityDescription', (), {'__init_subclass__': lambda **kw: None})
 
-ha_core.HomeAssistant = object
-ha_core.ServiceCall = object
-ha_config_entries.ConfigEntry = object
-ha_helpers.entity = ha_entity
-ha_helpers.entity_platform = ha_entity_platform
-ha_entity.EntityDescription = object
-ha_entity_platform.AddEntitiesCallback = object
-class _Subscriptable(type):
-    def __getitem__(cls, item):
-        return cls
+import homeassistant.helpers.update_coordinator as ha_uc
+ha_uc.CoordinatorEntity = type('CoordinatorEntity', (), {'__class_getitem__': classmethod(lambda cls, x: cls)})
 
-class _CoordinatorEntity(metaclass=_Subscriptable):
-    pass
-
-class _DataUpdateCoordinator(metaclass=_Subscriptable):
-    pass
-
-ha_update_coordinator.CoordinatorEntity = _CoordinatorEntity
-ha_update_coordinator.DataUpdateCoordinator = _DataUpdateCoordinator
-ha_update_coordinator.UpdateFailed = type('UpdateFailed', (Exception,), {})
-
-ha_exceptions = types.ModuleType('homeassistant.exceptions')
-ha_exceptions.ConfigEntryAuthFailed = type('ConfigEntryAuthFailed', (Exception,), {})
-
-ha_device_registry = types.ModuleType('homeassistant.helpers.device_registry')
-ha_device_registry.DeviceInfo = dict
-
-ha_cv = types.ModuleType('homeassistant.helpers.config_validation')
-ha_cv.string = str
-ha_cv.boolean = bool
-ha_event = types.ModuleType('homeassistant.helpers.event')
-ha_event.async_call_later = lambda *a, **k: None
-ha_http = types.ModuleType('homeassistant.components.http')
-
-sys.modules['homeassistant.helpers.config_validation'] = ha_cv
-sys.modules['homeassistant.helpers.event'] = ha_event
-sys.modules['homeassistant.components.http'] = ha_http
-
-sys.modules['homeassistant'] = ha_mock
-sys.modules['homeassistant.util'] = ha_util
-sys.modules['homeassistant.util.dt'] = ha_dt
-sys.modules['homeassistant.components'] = ha_components
-sys.modules['homeassistant.components.sensor'] = ha_sensor
-sys.modules['homeassistant.exceptions'] = ha_exceptions
-sys.modules['homeassistant.helpers.device_registry'] = ha_device_registry
-sys.modules['homeassistant.core'] = ha_core
-sys.modules['homeassistant.config_entries'] = ha_config_entries
-sys.modules['homeassistant.helpers'] = ha_helpers
-sys.modules['homeassistant.helpers.entity'] = ha_entity
-sys.modules['homeassistant.helpers.entity_platform'] = ha_entity_platform
-sys.modules['homeassistant.helpers.update_coordinator'] = ha_update_coordinator
-
-from pydoglog.models import DogEvent, EventType
 from custom_components.doglog.sensor import _to_datetime, _count_today, _get_most_recent_event
 
 
